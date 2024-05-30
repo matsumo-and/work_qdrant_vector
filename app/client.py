b6c62b78f3d665108f2c012e9eb15445d39b2c40
matsumo-and <yh134.toisanda@gmail.com>
2024-05-31 02:36:09 +0900

    [feat]finally-impl-it!!

--- app/client.py
+++ app/client.py
@@ -1,81 +1,68 @@
-from ast import literal_eval
-from dotenv import load_dotenv
-load_dotenv()
-
-import os
-import qdrant_client
+from typing import List
+import numpy as np
 import pandas as pd
+from fastembed.embedding import TextEmbedding
 from qdrant_client import QdrantClient
-from qdrant_client.models import Distance, VectorParams
-from qdrant_client.http import models as rest
-import openai
-
-import json
-
-OPEN_AI_API_KEY = os.getenv('OPENAIAPIKEY')
-
-#embeddings = OpenAIEmbeddings()
-
-df = pd.read_csv('./output.csv')
-df["embeddings"] = df.embeddings.apply(literal_eval)
-
-client = qdrant_client.QdrantClient(
-    host="qdrant",
-    prefer_grpc=True,
-)
-
-response = client.get_collections()
-print(response)
-
-client = QdrantClient(":memory:")
-
-vector_size = len(df["embeddings"][0])
-
-client.recreate_collection(
-    collection_name="HistoricalFigures",
-    vectors_config={
-        "content": rest.VectorParams(
-            distance=rest.Distance.COSINE,
-            size=vector_size,
-        ),
-    }
-)
-
-client.upsert(
-    collection_name="HistoricalFigures",
-    points=[
-        rest.PointStruct(
-            id=k,
-            vector={
-                "content": v["embeddings"],
-            },
-            payload=v.to_dict(),
-        )
-        for k, v in df.iterrows()
-    ],
-)
-# 全てのポイントが保存されていることを確認するためにコレクションのサイズを確認
-client.count(collection_name="HistoricalFigures")
-
-results = query_qdrant("最も偉大なシンガーは？", "HistoricalFigures")
-for i, content in enumerate(results):
-    json_obj = json.loads(content.payload["properties"])
-    name = json_obj["person_name"]
-    print(f"{i + 1}. {name} (Score: {round(content.score, 3)})")
 
 
-def query_qdrant(query, collection_name, vector_name="content", top_k=10):
-    embedded_query = openai.Embedding.create(
-        input=query,
-        model="text-embedding-ada-002",
-    )["data"][0]["embedding"]
-    
-    results = client.search(
-        collection_name=collection_name,
-        query_vector=(
-            vector_name, embedded_query
-        ),
-        limit=top_k,
+if __name__ == "__main__":
+
+    # Example list of documents
+    documents: List[str] = [
+        "ハローワールド",
+        "これはサンプルのドキュメントです。",
+        "fastembed は Qdrant がサポート・保守を行っています。",
+    ]
+
+    # embedding_model = Embedding(model_name="intfloat/multilingual-e5-large", max_length=512)
+    embedding_model = TextEmbedding()
+    embeddings: List[np.ndarray] = list(embedding_model.embed(documents))
+
+    for i in embeddings:
+        print(i.shape)
+        print(i[:5])
+
+
+
+
+    # Initialize the client
+    client = QdrantClient("qdrant", port=6333)
+
+    # set model
+    # client.set_model("intfloat/multilingual-e5-large")
+    print(f"vector params = {client.get_fastembed_vector_params()}")
+
+    client.recreate_collection(
+        collection_name="artists_collection",
+        vectors_config=client.get_fastembed_vector_params()
     )
 
-    return results
+    # import csv
+    df = pd.read_csv('./output.csv')
+
+    # ids
+    ids = df[df.columns[0]].tolist()
+
+    # metadata
+    metadata = df.drop('detail', axis=1).to_dict(orient='records')
+
+    # document
+    documents = df[df.columns[2]].tolist()
+
+    client.add(
+        collection_name="artists_collection",
+        documents=documents,
+        metadata=metadata,
+        ids=ids,
+        parallel=0
+    )
+
+    print("----------------------------------")
+    print("---------- set up done! ----------")
+    print("----------------------------------")
+
+    search_result = client.query(
+        collection_name="artists_collection",
+        query_text="best drummer."
+    )
+    print(search_result)
